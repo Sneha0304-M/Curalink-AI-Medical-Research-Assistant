c35952ae142d93e311168ca2b6aa70d032380ecf
Snehal Mathure <snehalmathure5@gmail.com>
2026-04-18 23:31:30 +0530

    fix production api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://curalink-backend-by6m.onrender.com/api",
   timeout: 60000,
 });
 
